540c962ce4eef0864911a84e249364479d39d2a2
vs4dhak <vedasadhak@gmail.com>
2024-03-06 19:23:16 -0500

    Updated version and readme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='auto_cmake',
-    version='2024.03.15',
+    version='2024.03.16',
     author='Veda Sadhak',
     author_email='vedasadhak@gmail.com',
     description='Automate setup of CMake projects',
